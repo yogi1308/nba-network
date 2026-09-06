7ae122388a4dd54c34aba6ad7bd58a487dcc41f2
yogi1308 <shreetej.yogi@gmail.com>
2026-09-06 13:24:03 -0700

    added tailwind

---
+++ site/components/RightPanel.jsx
@@ -0,0 +1,7 @@
+export default function Rightpanel() {
+    return (
+        <div className="flex h-[100vh] m-4 rounded-xl bg-black">
+        </div>
+    )
+    
+}
--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css'
-import SigmaCanvas from './graph/SigmaCanvas'
+import Rightpanel from "../components/RightPanel";
+import SigmaCanvas from "./graph/SigmaCanvas";
 
 export default function App() {
-  return (
-      <SigmaCanvas />
-  )
+    return (
+        <>
+            <SigmaCanvas />
+            <Rightpanel />
+        </>
+    );
 }
